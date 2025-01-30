91cffcc33b054b897130674f5b76df029703d8e7
윤효진 <hyojinee@dgsw.hs.kr>
2025-01-30 19:07:55 +0900

    😊😊

---
+++ 1002.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+        String h = "Hello";
+        String w = "World";
+        System.out.printf("%s %s", h,w);
+    }
+}
---
+++ 1003.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String h = "Hello";
+        String w = "World";
+        System.out.printf("%s\n%s", h, w);
+    }
+}
